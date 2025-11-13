b89556e88f3696d404b3dfef2526bd2f54c0cf8e
PawanGaria <pawangaria@192.168.1.11>
2025-11-14 00:55:13 +0530

    final commit

--- delivery/delivery/settings.py
+++ delivery/delivery/settings.py
@@ -203,6 +203,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://delivery-b5az.vercel.app/"
 ]
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "authorization",
